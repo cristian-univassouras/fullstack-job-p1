96b9207771904cc44a7dc6e12266cdf45e941942
cristianSbarboza <cristianbsconcursos@gmail.com>
2026-09-16 21:29:08 -0300

    create and write file urls in biblioteca

--- acervo/urls.py
+++ acervo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio)
+    path(
+        'livros', 
+         views.lista_livros,
+         name='lista'
+         ),
 ]
