e8570881ff91073942a45d031cb6b35ffb388147
cristianSbarboza <cristianbsconcursos@gmail.com>
2026-09-23 19:26:40 -0300

    registra novas entidades no admin

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Autor, Livro, Exemplar, Membro, Emprestimo, Reserva
 
+admin.site.register(Autor)
 admin.site.register(Livro)
-
+admin.site.register(Exemplar)
+admin.site.register(Membro)
+admin.site.register(Emprestimo)
+admin.site.register(Reserva)
