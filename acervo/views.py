9165214b667177d2cbfd874ec571dc350c6df205
cristianSbarboza <cristianbsconcursos@gmail.com>
2026-09-23 13:04:35 -0300

    adiciona view novo_livro para cadastro de livros

--- acervo/views.py
+++ acervo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Livro
+from .forms import LivroForm
 
 def lista_livros(request):
     livros = Livro.objects.all()
@@ -8,3 +9,14 @@ def lista_livros(request):
         request, 'acervo/lista.html',
         {'livros': livros}
     )
+
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+
+    else:
+        form = LivroForm()
+    return render(request, 'acervo/form.html', {'form': form})
