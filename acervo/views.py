b7361eebeaa140cff57fb1f3e1ec29444f28a4cb
cristianSbarboza <cristianbsconcursos@gmail.com>
2026-09-23 16:05:32 -0300

    adiciona busca por nome, tipo e categoria na listagem

--- acervo/views.py
+++ acervo/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Livro
 from .forms import LivroForm
 
 def lista_livros(request):
     livros = Livro.objects.all()
 
+    nome = request.GET.get('nome', '')
+    tipo = request.GET.get('tipo', '')
+    categoria = request.GET.get('categoria', '')
+
+    if nome:
+        livros = livros.filter(Q(titulo__icontains=nome) | Q(autor__icontains=nome))
+    if tipo:
+        livros = livros.filter(tipo_acervo=tipo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+
     return render(
         request, 'acervo/lista.html',
-        {'livros': livros}
+        {
+            'livros': livros,
+            'tipos': Livro.TIPO_ACERVO_CHOICES,
+            'categorias': Livro.CATEGORIA_CHOICES,
+            'filtro_nome': nome,
+            'filtro_tipo': tipo,
+            'filtro_categoria': categoria,
+        }
     )
 
 def novo_livro(request):
